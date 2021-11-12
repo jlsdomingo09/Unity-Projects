2d17d6ed191031416b119af66145b2d890923d62
jlsdomingo09 <83165924+jlsdomingo09@users.noreply.github.com>
2021-11-12 12:55:04 +0800

    Unity3D-Running-Man

---
+++ 3D-Running-Man/Assets/Scripts/enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemy : MonoBehaviour
+{
+   public GameObject target; // player
+    UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (target == null)
+            target = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+    
+    public void OnCollisionEnter(Collision collision) {
+        
+        if (collision.gameObject.tag == "Player")
+            SceneManager.LoadScene("Game");
+    }
+}
